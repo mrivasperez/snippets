c722a758569c9ad287289012ab27a2e490b44c56
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-30 11:30:46 -0700

    ui: Scaffold `create` page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="container mx-auto px-12">{children}</div>
+      </body>
     </html>
   );
 }
