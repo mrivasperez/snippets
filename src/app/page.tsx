7dedc3156ed52504a5b9a1df9e49eed485eebfd0
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-30 11:58:51 -0700

    feat: Fetch snippets in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div></div>;
-}
+import { db } from "@/db";
+
+const Home = async () => {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+
+  return (
+    <div>
+      <h1 className="text-3xl">snippets: {snippets.length}</h1>
+      {renderedSnippets}
+    </div>
+  );
+};
+
+export default Home;
