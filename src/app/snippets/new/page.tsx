86cb62d81a95367520eb4c5fd5dafca764d80ea9
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-30 11:42:12 -0700

    feat: Create server action to create code snippet

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,8 +1,26 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const SnippetCreatePage = () => {
+const CreateSnippetPage = () => {
+  const createSnippet = async (formData: FormData) => {
+    "use server";
+    // validate input
+    const title = formData.get("title") as string; // TODO improve checking
+    const code = formData.get("code") as string; // TODO improve checking
+
+    // create new db record
+    const newSnippet = await db.snippet.create({
+      data: { title, code }
+    });
+
+    console.log(newSnippet); // TODO
+
+    redirect("/");
+  };
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold my-10">Create a Code Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -34,4 +52,4 @@ const SnippetCreatePage = () => {
   );
 };
 
-export default SnippetCreatePage;
+export default CreateSnippetPage;
