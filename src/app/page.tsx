40bb5ba013a8872b05a9fe3fd35a6703afdf46b3
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-10-01 07:57:35 -0700

    feat: list code snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 const Home = async () => {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
   return (
     <div>
-      <h1 className="text-3xl">snippets: {snippets.length}</h1>
-      {renderedSnippets}
+      <div className="flex justify-between items-center my-4">
+        <h1 className="text-3xl">Snippets</h1>
+        <Link href={"/snippets/new"} className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+
+      <div className="flex gap-2 flex-col">{renderedSnippets}</div>
     </div>
   );
 };
