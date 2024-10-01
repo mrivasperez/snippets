49f59972803dfb2e69751a9bfdaf5b6c8a9b13ee
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-10-01 08:32:45 -0700

    task: Make editor controlled component with state

--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -2,21 +2,28 @@
 
 import { Editor } from "@monaco-editor/react";
 import { Snippet } from "@prisma/client";
-import React from "react";
+import React, { useState } from "react";
 
 interface EditSnippetFormProps {
   snippet: Snippet;
 }
 
 const EditSnippetForm = ({ snippet }: EditSnippetFormProps) => {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
   return (
     <div>
       <Editor
         height="40vh"
         language="javascript"
         theme="vs-dark"
-        defaultValue={snippet.code}
+        defaultValue={code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </div>
   );
