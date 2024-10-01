0bdf749679e5b31b23a0c54939ec847a7b4941de
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-10-01 07:49:39 -0700

    feat: create no-found page for viewing snippets

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SnippetNotFound = () => {
+  return (
+    <div>
+      <h1 className="text-xl bold">Code snippet could not be found.</h1>
+    </div>
+  );
+};
+
+export default SnippetNotFound;
